dcbd0b0a257bd867be60b48b2f3ccbbaf4d47ec4
RenanBicalho <renanbicalho@icloud.com>
2018-04-25 11:46:46 -0300

    25/04/2018 - 11:46

--- hdboot.php
+++ hdboot.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>HelpDesk Cartorial</title>
+  <title>CRIATOS - Manual</title>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
